c06f9e49b6d5ed4394c33e67df35c63e79b5e146
Gaurav Nelson <gaurav@stackrox.com>
2020-01-10 20:49:29 +1000

    Fix for links with spaces or special characters

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ module.exports = function asciidocLinkExtractor(asciidocFile) {
         var extractLink = asciidocFile.match(/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm);
         for (var i = 0; i < extractLink.length; i++) {
             var newLink = extractLink[i];
+            // fix for links with spaces or special characters
+            if (newLink.slice(-2) === "++") { 
+                newLink = newLink.slice(0, -2)
+            }
+            // fix end
             links.push(newLink);
         }
     }
